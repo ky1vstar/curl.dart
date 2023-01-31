bb9f21f98b5b5281aef9562079a4a86abe702364
Oleksandr Zhuhan <i@ky1vstar.dev>
2023-02-01 00:20:59 +0100

    Add `curl_flutter_libs` plugin

---
+++ curl_flutter_libs/lib/curl_flutter_libs.dart
@@ -0,0 +1,3 @@
+/// The purpose of the `curl_flutter_libs` package is to provide `sqlite3`
+/// native libraries when building Flutter apps.
+library sqlite3_flutter_libs;
---
+++ curl_flutter_libs/windows/curl_flutter_libs_plugin_c_api.cpp
@@ -0,0 +1,9 @@
+#include "include/curl_flutter_libs/curl_flutter_libs_plugin_c_api.h"
+
+#include <flutter/plugin_registrar_windows.h>
+
+#include "curl_flutter_libs_plugin.h"
+
+void CurlFlutterLibsPluginCApiRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+}
